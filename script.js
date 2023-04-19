2ac6aa815e701093ad04bb4dfd7f66e79af81b01
Juan Cruz <juan991396@gmail.com>
2023-04-19 04:50:18 -0300

    Se cambio el nombre de "index.html" a "juego.html" Se crearon las funciones de "escogerPalabra" y "dibujarGuiones"

--- script.js
+++ script.js
@@ -1,4 +1,52 @@
 var errores = 0;
+var palabrasecreta = "";
+var palabrasDisponibles = ['gato', 'perro', 'elefante', 'jirafa', 'leon'];
+
+// Función que escoge una palabra aleatoria de un arreglo de palabras disponibles
+function escogerPalabra(palabrasDisponibles) {
+    // Obtiene un índice aleatorio a partir del arreglo de palabras disponibles
+    let indiceAleatorio = Math.floor(Math.random() * palabrasDisponibles.length);
+    // Asigna la palabra seleccionada al valor de la variable "palabrasecreta"
+    palabrasecreta = palabrasDisponibles[indiceAleatorio];
+}
+
+// Función que dibuja guiones en el canvas para cada letra de la palabra secreta
+function dibujarGuiones(palabrasecreta){
+    // Obtiene la referencia al canvas de la pantalla y crea un contexto de dibujo 2D
+    let pantalla = document.getElementById("palabra");
+    let pincel = pantalla.getContext("2d");
+
+    // Establece el color y el tamaño del trazo
+    pincel.lineWidth = 5;
+    pincel.strokeStyle = "darkblue";
+
+    // Divide la palabra secreta en un arreglo de letras
+    var letras = palabrasecreta.split("");
+
+    // Dibuja dos guiones por letra en el canvas
+    for(let i = 1; i <= 2*letras.length; i=i+2){
+        let t = 30 *i;
+        // Dibuja el guión oscuro
+        pincel.strokeStyle = "darkblue";
+        pincel.beginPath();
+        pincel.moveTo(t, 50);
+        pincel.lineTo(t+50, 50);
+        pincel.stroke(); 
+
+        // Dibuja un espacio en blanco
+        pincel.strokeStyle = "white";
+        pincel.beginPath();
+        pincel.moveTo(t+50, 50);
+        pincel.lineTo(t+100, 50);
+        pincel.stroke(); 
+    }
+}
+//prueba
+escogerPalabra(palabrasDisponibles);
+console.log(palabrasecreta);
+dibujarGuiones(palabrasecreta);
+//prueba
+
 function dibujar(){
     let pantalla = document.getElementById("ahorcado");
     let pincel = pantalla.getContext("2d");
